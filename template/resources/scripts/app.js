d20faef1e9f34222b9f63fa4fe935fe302ed4a6d
Damie <damienmarble@gmail.com>
2019-02-23 23:09:21 -0500

    More instruction.

--- template/resources/scripts/app.js
+++ template/resources/scripts/app.js
@@ -2,8 +2,14 @@
 Replace this file with the entry point for your app.
 
 Your app must implement the following API:
-attach a function named `character` to the `window` object. This method takes one optional argument.
+When the app loads, it initializes with a brand new character.
+
+Attach a function named `character` to the `window` object. This method takes one optional argument.
 - If called with undefined, `character` acts as a getter. Return a JSON stringified form of your current character data.
 - If called with an argument, `character` acts as a setter. The argument is the persisted JSON string of your character. Parse and use it to set your character state.
+
+Optionally, attach a function named 'export' to the 'window' object. This enables exporting to PDF.
+- If called, returns the JSON stringified state of your current character.
+
  */
 console.log('hello world!');
