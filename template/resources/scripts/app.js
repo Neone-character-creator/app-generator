64e83ab8e1e897eeac372aa53c1adebf784f1420
Damie <damienmarble@gmail.com>
2019-02-23 23:02:13 -0500

    Added comments to starting app.js.

--- template/resources/scripts/app.js
+++ template/resources/scripts/app.js
@@ -1,1 +1,9 @@
+/*
+Replace this file with the entry point for your app.
+
+Your app must implement the following API:
+attach a function named `character` to the `window` object. This method takes one optional argument.
+- If called with undefined, `character` acts as a getter. Return a JSON stringified form of your current character data.
+- If called with an argument, `character` acts as a setter. The argument is the persisted JSON string of your character. Parse and use it to set your character state.
+ */
 console.log('hello world!');
