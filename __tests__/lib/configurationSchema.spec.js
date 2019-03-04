fc72954860cc1aba5b96853e72cf7dab1771b350
Damie <damienmarble@gmail.com>
2019-03-04 17:09:05 -0500

    Refactoring with additional test.

--- __tests__/lib/configurationSchema.spec.js
+++ __tests__/lib/configurationSchema.spec.js
@@ -1,22 +1,45 @@
 const configSchema = require('../../lib/configurationSchema');
 describe("the configuration schema", () => {
     describe("the summary view", () => {
-       it("has a default 'name' of Character", done => {
+        it("is created if not set in the views", () => {
+           const config = {
+               views: {}
+           };
+            expect.assertions(2);
+            return configSchema(config).then(validated => {
+                expect(validated.views.summary).toBeTruthy();
+                expect(validated.views.summary.name).toBe("Character");
+            });
+        });
+       it("has a default 'name' of Character", () => {
            const config = {
                views: {
                    summary: {}
                }
            };
            expect.assertions(2);
-           configSchema(config).then(validated => {
+           return configSchema(config).then(validated => {
                expect(validated.views.summary).toBeTruthy();
                expect(validated.views.summary.name).toBe("Character");
-               done();
            });
        }) ;
+       it("can override the name by setting the property", () => {
+           const config = {
+               views: {
+                   summary: {
+                       name: "Replacement"
+                   }
+               }
+           };
+           expect.assertions(2);
+           return configSchema(config).then(validated => {
+               expect(validated.views.summary).toBeTruthy();
+               expect(validated.views.summary.name).toBe("Replacement");
+           }).catch(fail);
+       });
     });
     describe("the non-summary views", () => {
-        it("must have a name field", done => {
+        it("must have a name field", () => {
             const config = {
                 views: {
                     summary: {},
@@ -24,9 +47,8 @@ describe("the configuration schema", () => {
                 }
             };
             expect.assertions(1);
-            configSchema(config).catch(e => {
+            return configSchema(config).catch(e => {
                 expect(e.message).toBe("views.one.name is a required field");
-                done();
             });
         });
     });
