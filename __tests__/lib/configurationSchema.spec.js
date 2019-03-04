df1c9e99f9e5f25bf5ec0cbf65881db0bb23ea1f
Damie <damienmarble@gmail.com>
2019-03-04 16:48:52 -0500

    Refactored and removed some non-unneeded tests.

--- __tests__/lib/configurationSchema.spec.js
+++ __tests__/lib/configurationSchema.spec.js
@@ -1,28 +1,5 @@
 const configSchema = require('../../lib/configurationSchema');
 describe("the configuration schema", () => {
-    it("requires the configuration to have a views top-level element", done => {
-        const config = {};
-        configSchema(config).validate(config, {
-            strict: true
-        }).catch(e => {
-            expect(e.message).toBe("views is a required field");
-            done();
-        });
-    });
-    it("requires views to contain at least the summary view", done => {
-        const config = {
-            views: {
-                summary: {}
-            }
-        };
-        expect.assertions(1);
-        return configSchema(config).validate(config, {
-            strict: true
-        }).then(validated => {
-            expect(validated).toBeTruthy();
-            done();
-        }).catch(e => console.log(e));
-    });
     describe("the summary view", () => {
        it("has a default 'name' of Character", done => {
            const config = {
@@ -31,7 +8,7 @@ describe("the configuration schema", () => {
                }
            };
            expect.assertions(2);
-           configSchema(config).validate(config).then(validated => {
+           configSchema(config).then(validated => {
                expect(validated.views.summary).toBeTruthy();
                expect(validated.views.summary.name).toBe("Character");
                done();
@@ -47,9 +24,7 @@ describe("the configuration schema", () => {
                 }
             };
             expect.assertions(1);
-            configSchema(config).validate(config, {
-                strict: true
-            }).catch(e => {
+            configSchema(config).catch(e => {
                 expect(e.message).toBe("views.one.name is a required field");
                 done();
             });
