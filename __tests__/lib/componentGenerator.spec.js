658e8b39839fd2c43a12e186d6612a682e0109e6
Damie <damienmarble@gmail.com>
2019-03-10 04:45:31 -0400

    Added module for generating all components at once.

---
+++ __tests__/lib/componentGenerator.spec.js
@@ -0,0 +1,32 @@
+const componentGenerator = require("../../lib/componentGenerator");
+const sinon = require("sinon");
+const renderer = require("react-test-renderer");
+
+describe("component generator module", () =>{
+    let componentWriter;
+    beforeEach(()=>{
+        componentWriter = sinon.stub()
+    });
+
+    it("writes an app component", async () => {
+        const hierarchy = {
+            appName: "test",
+            views: {
+                one: {
+                    children: ["foo"]
+                }
+            },
+            components: {
+                foo: {
+                    type: "textfield"
+                }
+            }
+        };
+        const components = await componentGenerator(hierarchy);
+        expect(components.app.path).toBe('/app.js');
+        expect(renderer.create(components.app.content).toJSON()).toMatchSnapshot();
+
+        expect(components.views.one.path).toBe('/views/one.js');
+        expect(renderer.create(components.views.one.content).toJSON()).toMatchSnapshot();
+    });
+});
