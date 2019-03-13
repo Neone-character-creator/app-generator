d966f0a87b27a299a6a39d86d67e2f86c4600734
Damie <damienmarble@gmail.com>
2019-03-12 21:58:57 -0400

    Integration test for component model generation.

---
+++ __tests__/configuration.integration.spec.js
@@ -0,0 +1,35 @@
+const fs = require("fs-extra");
+const path = require("path");
+
+describe("plugin generation chain", () => {
+   it("completes", async done => {
+       try {
+           const configFile = await require("../lib/configurationSchema")(JSON.parse(fs.readFileSync(path.resolve(__dirname, `test.json`), 'utf-8')));
+           const hierarchy = require('../lib/generateComponentsHierarchy').default(configFile);
+           const components = await require('../lib/componentGenerator')(hierarchy);
+           expect(components.views[0]).toBe("summary");
+           expect(components.components.summary).toEqual({
+               type: "view",
+               children: [
+                   "biography",
+                   "combat",
+                   "characteristics",
+                   "skills"
+               ]
+           });
+
+           expect(component.components.biography).toEqual({
+               type: "container",
+               direction: "vertical",
+               children: [
+                   "name",
+                   "species",
+                   "career",
+                   "specializations"
+               ]
+           })
+       } catch (e) {
+           done(e);
+       }
+   });
+});
