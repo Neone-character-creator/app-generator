d03746f42db0e163e6ca5445e2af4949796240b4
Damie <damienmarble@gmail.com>
2019-03-13 21:22:21 -0400

    Move configuration loading into app.js.

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const argv = require('yargs').argv;
 const fs = require('fs-extra');
 
-if (!argv.projectName) {
-    throw new Error("--projectName missing");
+if (!argv.configFile) {
+    throw new Error("--configFile is missing.");
 }
 
-const projectDirectory = argv.projectName;
+const configFile = require("./lib/configurationSchema")(JSON.parse(fs.readFileSync(argv.configFile, 'utf-8')));
+const configuration = require('./lib/generateComponentsHierarchy').default(configFile);
 
-const clean = !fs.existsSync(`plugin-${projectDirectory}`);
+const clean = !fs.existsSync(`plugin-${configuration.appName}`);
 
 if (!clean) {
     throw new Error('Output directory already exists. Delete it or choose a different project name.');
 }
-require('./lib/pluginGenerator')(argv.projectName, argv.scaffold).catch(e => console.error(e));
+
+require('./lib/pluginGenerator')(configuration, argv.scaffold).catch(e => console.error(e));
