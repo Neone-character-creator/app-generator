1e6c3d13006bb9ffda5f5f20c052ad10fd082dfc
Damie <damienmarble@gmail.com>
2019-03-18 21:46:56 -0400

    Updated demo serve.js.

--- project-template/serve.js
+++ project-template/serve.js
@@ -1,13 +1,15 @@
 const app = require('express')();
-
+const path = require("path");
 const port = 9999;
 
-app.use('/src/resources/templates/sheet.html', function(req, res) {
-    res.sendFile(__dirname + '/src/resources/templates/sheet.html');
+
+app.use('/demo', function(req, res, next){
+    res.sendFile(__dirname + '/demo.html');
 });
 
-app.use('/', function(req, res, next){
-    res.sendFile(__dirname + '/demo.html');
+app.use('/*', function(req, res) {
+    const resourcePath = path.resolve(__dirname + "/src/main/resources" + req.baseUrl);
+    res.sendFile(resourcePath);
 });
 
 app.listen(port, ()=>{
