70c0311f97fa2b8702f44da738610dced5f81ebc
Damie <damienmarble@gmail.com>
2019-04-26 21:11:52 -0400

    Additional schema validation.

---
+++ __tests__/lib/schema/configurationSchema.spec.js
@@ -0,0 +1,98 @@
+const configSchema = require('../../../lib/schema/configuration');
+const textfieldSchema = require("../../../lib/schema/component/textfield");
+describe("the configuration schema", () => {
+    describe("textfield", () => {
+        it("must have a type", () => {
+            const config = {};
+            expect.assertions(2);
+            try {
+                textfieldSchema(config).validateSync(config);
+            } catch(e) {
+                expect(e.errors.length).toEqual(1);
+                expect(e.errors[0]).toBe("type is a required field");
+            }
+        });
+        it("type must be textfield", () => {
+            const config = {
+                type: "foobar"
+            };
+            expect.assertions(2);
+            try {
+                textfieldSchema(config).validateSync(config);
+            } catch(e) {
+                expect(e.errors.length).toEqual(1);
+                expect(e.errors[0]).toBe("type must be one of the following values: textfield");
+            }
+        });
+        it("may not have unknown properties", () => {
+            const config = {
+                type: "textfield",
+                foo: "bar"
+            };
+            expect.assertions(2);
+            try {
+                textfieldSchema(config).validateSync(config);
+            } catch(e) {
+                expect(e.errors.length).toEqual(1);
+                expect(e.errors[0]).toBe("this field cannot have keys not specified in the object shape");
+            }
+        });
+        it("may have a binding expression", () => {
+            const config = {
+                type: "textfield",
+                bind: "to something"
+            };
+            try {
+                textfieldSchema(config).validateSync(config);
+            } catch(e) {
+                fail(e);
+            }
+        });
+    });
+    describe("the views", () => {
+        it("must have a name field", () => {
+            const config = {
+                views: {
+                    children: {
+                        summary: {
+                            children: {}
+                        },
+                        one: {
+                            children: {}
+                        }
+                    }
+                }
+            };
+            expect.assertions(3);
+            try {
+                configSchema(config)
+            } catch (e) {
+                expect(e.errors.length).toEqual(2);
+                expect(e.errors[0]).toBe("views.children.summary.name is a required field");
+                expect(e.errors[1]).toBe("views.children.one.name is a required field");
+            }
+        });
+        it("must have children", () => {
+            const config = {
+                views: {
+                    children: {
+                        summary: {
+                            name: "Summary"
+                        },
+                        one: {
+                            name: "One"
+                        }
+                    }
+                }
+            };
+            expect.assertions(3);
+            try {
+                configSchema(config)
+            } catch (e) {
+                expect(e.errors.length).toEqual(2);
+                expect(e.errors[0]).toBe("views.children.summary.children is a required field");
+                expect(e.errors[1]).toBe("views.children.one.children is a required field");
+            }
+        })
+    });
+});
