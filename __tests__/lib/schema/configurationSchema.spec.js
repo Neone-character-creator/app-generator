54e07b936fadf7d0d4bbfb77b6e18a3649a06ff4
Damie <damienmarble@gmail.com>
2019-04-26 23:23:58 -0400

    Tests for containers.

--- __tests__/lib/schema/configurationSchema.spec.js
+++ __tests__/lib/schema/configurationSchema.spec.js
@@ -152,6 +152,60 @@ describe("the configuration schema", () => {
             }
         });
     });
+    describe("container", () => {
+        it("must have a type", () => {
+            const config = {};
+            expect.assertions(2);
+            try {
+                containerSchema(config).validateSync(config, {
+                    abortEarly: false
+                });
+            } catch(e) {
+                expect(e.errors.length).toBeGreaterThanOrEqual(1);
+                expect(e.errors).toContain("type is a required field");
+            }
+        });
+        it("type must be container", () => {
+            const config = {
+                type: "foobar"
+            };
+            expect.assertions(2);
+            try {
+                containerSchema(config).validateSync(config, {
+                    abortEarly: false
+                });
+            } catch(e) {
+                expect(e.errors.length).toBeGreaterThanOrEqual(1);
+                expect(e.errors).toContain("type must be one of the following values: container");
+            }
+        });
+        it("it must have at least one child", () => {
+            const config = {
+                type: "container",
+                children: {}
+            };
+            expect.assertions(2);
+            try {
+                containerSchema(config).validateSync(config);
+            } catch(e) {
+                expect(e.errors.length).toEqual(1);
+                expect(e.errors[0]).toBe("children must have at least one key");
+            }
+        });
+        it("may not have unknown properties", () => {
+            const config = {
+                type: "checkbox",
+                foo: "bar"
+            };
+            expect.assertions(2);
+            try {
+                containerSchema(config).validateSync(config);
+            } catch(e) {
+                expect(e.errors.length).toEqual(1);
+                expect(e.errors[0]).toBe("this field cannot have keys not specified in the object shape");
+            }
+        });
+    });
     describe("the views", () => {
         it("must have a name field", () => {
             const config = {
