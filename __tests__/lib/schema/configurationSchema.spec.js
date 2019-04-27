cbbc9c1cc8f8cba53c0802234d0c30f471f987e4
Damie <damienmarble@gmail.com>
2019-04-26 21:32:47 -0400

    More tests for container schema.

--- __tests__/lib/schema/configurationSchema.spec.js
+++ __tests__/lib/schema/configurationSchema.spec.js
@@ -1,6 +1,7 @@
 const configSchema = require('../../../lib/schema/configuration');
 const textfieldSchema = require("../../../lib/schema/component/textfield");
 const checkboxSchema = require("../../../lib/schema/component/checkbox");
+const containerSchema = require("../../../lib/schema/component/container");
 
 describe("the configuration schema", () => {
     describe("textfield", () => {
@@ -96,6 +97,58 @@ describe("the configuration schema", () => {
                 checkboxSchema(config).validateSync(config);
             } catch(e) {
                 fail(e);
+            }
+        });
+    });
+    describe("container", () => {
+        it("must have a type", () => {
+            const config = {};
+            expect.assertions(2);
+            try {
+                containerSchema(config).validateSync(config, {
+                    abortEarly: false
+                });
+            } catch(e) {
+                expect(e.errors.length).toBeGreaterThanOrEqual(1);
+                expect(e.errors).toContain("type is a required field");
+            }
+        });
+        it("type must be container", () => {
+            const config = {
+                type: "foobar"
+            };
+            expect.assertions(2);
+            try {
+                containerSchema(config).validateSync(config);
+            } catch(e) {
+                expect(e.errors.length).toEqual(1);
+                expect(e.errors[0]).toBe("children is a required field");
+            }
+        });
+        it("it must have at least one child", () => {
+            const config = {
+                type: "container",
+                children: {}
+            };
+            expect.assertions(2);
+            try {
+                containerSchema(config).validateSync(config);
+            } catch(e) {
+                expect(e.errors.length).toEqual(1);
+                expect(e.errors[0]).toBe("children must have at least one key");
+            }
+        });
+        it("may not have unknown properties", () => {
+            const config = {
+                type: "checkbox",
+                foo: "bar"
+            };
+            expect.assertions(2);
+            try {
+                containerSchema(config).validateSync(config);
+            } catch(e) {
+                expect(e.errors.length).toEqual(1);
+                expect(e.errors[0]).toBe("this field cannot have keys not specified in the object shape");
             }
         });
     });
