34dbd24c1d77eb17261865e94b42216e326ec200
Damie <damienmarble@gmail.com>
2019-04-28 21:42:13 -0400

    Wrap in try-catch to print errors.

--- app.js
+++ app.js
@@ -4,14 +4,19 @@ const fs = require('fs-extra');
 if (!argv.configFile) {
     throw new Error("--configFile is missing.");
 }
+try {
+    const configFile = require("./lib/schema/configuration")(JSON.parse(fs.readFileSync(argv.configFile, 'utf-8')));
+    const configuration = require('./lib/generateComponentsHierarchy').default(configFile);
 
-const configFile = require("./lib/schema/configuration")(JSON.parse(fs.readFileSync(argv.configFile, 'utf-8')));
-const configuration = require('./lib/generateComponentsHierarchy').default(configFile);
+    const clean = !fs.existsSync(`plugin-${configuration.appName}`);
 
-const clean = !fs.existsSync(`plugin-${configuration.appName}`);
+    if (!clean) {
+        throw new Error('Output directory already exists. Delete it or choose a different project name.');
+    }
 
-if (!clean) {
-    throw new Error('Output directory already exists. Delete it or choose a different project name.');
+    require('./lib/pluginGenerator')(configuration, argv.scaffold).catch(e => console.error(e));
+} catch (e) {
+    e.errors.forEach(error => {
+        console.error(error);
+    })
 }
-
-require('./lib/pluginGenerator')(configuration, argv.scaffold).catch(e => console.error(e));
