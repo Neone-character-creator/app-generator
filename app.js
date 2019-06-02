08fae0a04284223d8010d1e3432a8fa563ed4c04
Damie <damienmarble@gmail.com>
2019-06-02 09:31:08 -0400

    Begun implementing Model class generation.

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const argv = require('yargs').argv;
 const fs = require('fs-extra');
 const _ = require("lodash");
 const PluginGenerator = require("./lib/pluginGenerator");
+const ModelClassGenerator = require("./lib/classGenerator");
+const tmp = require("tmp");
 
 if (!argv.configFile) {
     throw new Error("--configFile is missing.");
@@ -10,8 +12,14 @@ try {
     const rawConfiguraiton = require("./lib/schema/configuration")(JSON.parse(fs.readFileSync(argv.configFile, 'utf-8')));
     const componentConfiguration = Object.assign(rawConfiguraiton, require('./lib/generateComponentsHierarchy').default(rawConfiguraiton));
     const modelConfiguration = _.pick(rawConfiguraiton, "model");
+    const appConfiguration = Object.assign({}, componentConfiguration, modelConfiguration);
 
-    var pluginGenerator = new PluginGenerator(Object.assign({}, componentConfiguration, modelConfiguration));
+    const tmpDir = tmp.dirSync();
+    if(argv.only === "classes") {
+        new ModelClassGenerator(appConfiguration.model, tmpDir).generate();
+    }
+
+    var pluginGenerator = new PluginGenerator(appConfiguration, tmpDir);
     pluginGenerator.generate().catch(e => console.error(e));
 } catch (e) {
     if (e.errors) {
