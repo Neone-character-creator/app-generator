023779b01e67178923a8c11ecec8bced91a22641
Damien Marble <damienmarble@gmail.com>
2019-06-08 09:53:00 -0700

    Removed references to transformer.

--- __tests__/lib/model/transformers/app.js
+++
@@ -1,7 +0,0 @@
-const transformer = require("../../../../lib/model/transformers/app");
-
-describe("app config transformer", () => {
-    it("throws an error if views undefined", () => {
-       expect(()=>transformer({})).toThrowErrorMatchingSnapshot();
-    });
-});
