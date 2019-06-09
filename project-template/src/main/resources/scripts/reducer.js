65aac285cf85948b2f91a17d8a668d1aa8af9a26
Damie <damienmarble@gmail.com>
2019-06-08 20:35:56 -0400

    Added support for calculated properties in reducer.

--- project-template/src/main/resources/scripts/reducer.js
+++ project-template/src/main/resources/scripts/reducer.js
@@ -1,27 +1,51 @@
 const _ = require("lodash");
 const models = require("./models");
+
+function setCalculatedProperties(modelDefinition, state, statePath){
+    if (modelDefinition.derivedFrom) {
+        let value = _.template(modelDefinition.derivedFrom)(state);
+        switch (modelDefinition.type) {
+            case "number":
+                value = Number.parseInt(value);
+                break;
+        }
+        _.set(state, statePath, value);
+    }
+    Object.getOwnPropertyNames(modelDefinition).reduce((updated, nextPropertyName) => {
+        const modelProperty = modelDefinition[nextPropertyName];
+        const propertyModelDefinition = models[modelDefinition[nextPropertyName].type] ? models[modelDefinition[nextPropertyName].type].prototype.definition : undefined;
+        const propertyPath = statePath + `['${nextPropertyName}']`;
+        if (modelProperty.derivedFrom) {
+            setCalculatedProperties(modelProperty, state, propertyPath);
+        } else if (propertyModelDefinition){
+            setCalculatedProperties(propertyModelDefinition, state, propertyPath);
+        }
+    }, state);
+}
+
 module.exports = function(previousState, action) {
-    if (action.type === "SET") {
-        _.set(previousState, action.path, action.value);
-        return {...previousState};
-    }
-    if(action.type === "REMOVE") {
-        const array = _.get(previousState, action.path);
-        if (!_.isArray(array)) {
-            throw new Error(`value at path ${action.path} is not array!`);
+    if(previousState) {
+        previousState = {...previousState};
+        if (action.type === "SET") {
+            _.set(previousState, action.path, action.value);
         }
-        array.splice(action.remove);
-        return {...previousState};
-    }
-    if(action.type === "ADD") {
-        const array = _.get(previousState, action.path);
-        if (!_.isArray(array)) {
-            throw new Error(`value at path ${action.path} is not array!`);
+        if (action.type === "REMOVE") {
+            const array = _.get(previousState, action.path);
+            if (!_.isArray(array)) {
+                throw new Error(`value at path ${action.path} is not array!`);
+            }
+            array.splice(action.remove);
         }
-        array.push(action.value);
-        return {...previousState};
+        if (action.type === "ADD") {
+            const array = _.get(previousState, action.path);
+            if (!_.isArray(array)) {
+                throw new Error(`value at path ${action.path} is not array!`);
+            }
+            array.push(action.value);
+        }
+    } else {
+        previousState = {character: new models.character()};
     }
-    return previousState || {
-        character: new models.character()
-    };
+    setCalculatedProperties(models.character.prototype.definition, previousState, "character");
+    return previousState;
 };
