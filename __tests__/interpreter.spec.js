c2e688b84202258a7b7d420f7389fb071f59f823
Damie <damienmarble@gmail.com>
2019-06-16 18:55:15 -0400

    Updated interpreter tests.

--- __tests__/interpreter.spec.js
+++ __tests__/interpreter.spec.js
@@ -1,35 +1,31 @@
 const Interpreter = require("../project-template/src/main/resources/scripts/interpreter");
 describe("embedded interpreter", () => {
-    it("evaluates a number literal to that value", () => {
-        const expression = "1";
+    it("can return a number literal", () => {
+        const expression = "return 1";
         expect(Interpreter.interpret(expression, {})).toEqual(1);
     });
-    it("evaluates a string literal to that string", () => {
-        const expression = "string";
-        expect(Interpreter.interpret(expression, {})).toEqual("string");
+    it("can return a string literal", () => {
+        const expression = "return 'string'";
+        expect(Interpreter.interpret(expression, {})).toEqual("'string'");
     });
-    it("evaluates addition", () => {
-        const expression = "1+1";
+    it("can return the result of addition", () => {
+        const expression = "return 1+1";
         expect(Interpreter.interpret(expression, {})).toEqual(2);
     });
-    it("evaluates multiplication", () => {
-        const expression = "2*2";
+    it("can return the result of multiplication", () => {
+        const expression = "return 2*2";
         expect(Interpreter.interpret(expression, {})).toEqual(4);
     });
-    it("evaluates division", () => {
-        const expression = "2/2";
+    it("can return the result of division", () => {
+        const expression = "return 2/2";
         expect(Interpreter.interpret(expression, {})).toEqual(1);
     });
-    it("rounds down when dividing", () => {
-        const expression = "3/2";
-        expect(Interpreter.interpret(expression, {})).toEqual(1);
-    });
-    it("evaluates chained addition", () => {
-        const expression = "1+1+1";
+    it("can return the result of chained addition", () => {
+        const expression = "return 1+1+1";
         expect(Interpreter.interpret(expression, {})).toEqual(3);
     });
     it("resolves state variables", () => {
-        const expression = "$state.foo";
+        const expression = "return $state.foo";
         const context = {
             $state: {
                 foo: 1
@@ -38,7 +34,7 @@ describe("embedded interpreter", () => {
         expect(Interpreter.interpret(expression, context)).toEqual(context.$state.foo);
     });
     it("resolves nested state variables", () => {
-        const expression = "$state.foo.bar.baz";
+        const expression = "return $state.foo.bar.baz";
         const context = {
             $state: {
                 foo: {
@@ -50,4 +46,17 @@ describe("embedded interpreter", () => {
         };
         expect(Interpreter.interpret(expression, context)).toEqual(12345);
     });
+    it("return undefined if there is no return statement", () => {
+        const expression = "1";
+        const context = {
+        };
+        expect(Interpreter.interpret(expression, context)).toBeUndefined();
+    });
+    it("can call methods", () => {
+        const expression = "return $state.join('+')";
+        const context = {
+            $state: ["foo", "bar"]
+        };
+        expect(Interpreter.interpret(expression, context)).toEqual("foo+bar");
+    })
 });
