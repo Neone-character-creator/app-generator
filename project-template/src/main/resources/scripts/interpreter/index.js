eb39fa9bbe3ac0c3e3fbcbd862792e0598e0487c
Damien Marble <damienmarble@gmail.com>
2019-08-24 12:19:05 -0400

    Add index to interpreter.

--- project-template/src/main/resources/scripts/interpreter/index.js
+++ project-template/src/main/resources/scripts/interpreter/index.js
@@ -2,7 +2,7 @@ import rules from "!../rules.json";
 export default {
     interpret: function(expression, context){
         if (typeof expression === "string" && expression.startsWith("#")) {
-            return new Function("$state", "$model", "$this", "$rules", `return ${expression.substring(1)}`)(context.$state, context.$model, context.$this, rules);
+            return new Function("$state", "$model", "$this", "$rules", "$index", `return ${expression.substring(1)}`)(context.$state, context.$model, context.$this, context.$index, rules);
         } else {
             return expression;
         }
