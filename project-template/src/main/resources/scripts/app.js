fc5369cfe433626a7479dd7b20c049039952d571
Damien Marble <damienmarble@gmail.com>
2019-08-31 10:57:34 -0400

    Add debugging library.

--- project-template/src/main/resources/scripts/app.js
+++ project-template/src/main/resources/scripts/app.js
@@ -2,10 +2,14 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
-
+import { whyDidYouUpdate } from "why-did-you-update";
 import reducer from "./reducer";
+import React from "react";
 
 const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+if (process.env.DEBUG) {
+    whyDidYouUpdate(React);
+}
 
 ReactDOM.render(
     <Provider store={store}>
