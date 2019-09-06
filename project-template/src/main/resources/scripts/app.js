b13ce475195669146f78ee8ce5b9e029909c70ac
Damie <damienmarble@gmail.com>
2019-09-06 11:47:28 -0400

    Change app to accept stringified state.

--- project-template/src/main/resources/scripts/app.js
+++ project-template/src/main/resources/scripts/app.js
@@ -7,16 +7,16 @@ import reducer from "./reducer";
 import React from "react";
 
 const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-window.character = function(state) {
+window.character = function(stringifiedState) {
     if (state === undefined) {
         return store.getState().character;
     } else {
         store.dispatch({
             type: "OVERRIDE",
-            state
+            state: JSON.parse(stringifiedState)
         });
     }
-}
+};
 if (process.env.NODE_ENV === "dev") {
     whyDidYouUpdate(React);
 }
