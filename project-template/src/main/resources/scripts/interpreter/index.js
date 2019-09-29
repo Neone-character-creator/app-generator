0b64fceb051dfd17bb6f72fff886e5ee705623e9
Damie <damienmarble@gmail.com>
2019-09-28 21:30:32 -0400

    Formatting.

--- project-template/src/main/resources/scripts/interpreter/index.js
+++ project-template/src/main/resources/scripts/interpreter/index.js
@@ -1,10 +1,11 @@
 import rules from "!../rules.json";
 import models from "../models";
+
 const interpreter = {
-    interpret: function(expression, context){
+    interpret: function (expression, context) {
         if (typeof expression === "string" && expression.startsWith("#")) {
             return new Function("$state", "$models", "$this", "$rules", "$index", `return ${expression.substring(1)}`)(context.$state, models, context.$this, rules, context.$index);
-        } else if(expression && (expression.any || expression.all)) {
+        } else if (expression && (expression.any || expression.all)) {
             expression.any
         } else {
             return expression;
