bfa187f0e983596fe47021a62c671f0a4b82d8bc
Damie <damienmarble@gmail.com>
2019-10-02 19:28:50 -0400

    Return values without a model.

--- project-template/src/main/resources/scripts/reducer/modelTranslator.js
+++ project-template/src/main/resources/scripts/reducer/modelTranslator.js
@@ -13,39 +13,47 @@ const modelTranslator = function (modelConfiguration, targetPath, value) {
             if (currentPosition === undefined) {
                 return undefined;
             }
+            if(/^\d+$/.test(nextPathElement)) {
+                return currentPosition;
+            }
             if (currentPosition.prototype) {
                 return currentPosition.prototype.definition[nextPathElement];
             } else {
                 return currentPosition[nextPathElement];
             }
         }, modelConfiguration);
-        const isArrayMatcher = /\[(.*)\]/.exec(modelDef.type);
-        const instanceModelType = isArrayMatcher ? isArrayMatcher[1] : modelDef.type;
-        if (instanceModelType === "string" || instanceModelType === "number") {
-            const arrayTypeButValueNotArray = isArrayMatcher && !_.isArray(value);
-            const arrayTypeButArrayValuesWrong = isArrayMatcher && instanceModelType !== typeof  value[0];
-            const typeWrong = instanceModelType === typeof value;
-            if (arrayTypeButValueNotArray || arrayTypeButArrayValuesWrong || typeWrong) {
-                throw new Error("Path " + targetPath + " has a defined type of " + modelDef.type + " but a " + typeof value + " was given.");
+        if(modelDef) {
+            const isArrayMatcher = /\[(.*)\]/.exec(modelDef.type);
+            const instanceModelType = isArrayMatcher ? isArrayMatcher[1] : modelDef.type;
+            if (instanceModelType === "string" || instanceModelType === "number") {
+                const arrayTypeButValueNotArray = isArrayMatcher && !_.isArray(value);
+                const arrayTypeButArrayValuesWrong = isArrayMatcher && instanceModelType !== typeof value[0];
+                const typeWrong = instanceModelType === typeof value;
+                if (arrayTypeButValueNotArray || arrayTypeButArrayValuesWrong || typeWrong) {
+                    throw new Error("Path " + targetPath + " has a defined type of " + modelDef.type + " but a " + typeof value + " was given.");
+                }
+                return value;
             }
+
+            return lookupOrCreateInstance(modelConfiguration, _.get(value, "id", value), modelDef);
+        } else {
             return value;
         }
-
-        return lookupOrCreateInstance(modelConfiguration, _.get(value, "id", value), modelDef);
     }
 ;
 
 export default modelTranslator;
 
 function lookupOrCreateInstance(modelConfiguration, value, typeDefinition) {
-    const lookupValue = modelConfiguration[typeDefinition.type.replace("[", "").replace("]", "")].values.find(v => v.id === value);
+    const instanceType = typeDefinition.type.replace("[", "").replace("]", "");
+    const lookupValue = modelConfiguration[instanceType].values.find(v => v.id === value);
     if (lookupValue) {
-        lookupValue.effects = [...modelConfiguration[typeDefinition.type].prototype.effects];
+        lookupValue.effects = [...modelConfiguration[instanceType].prototype.effects];
         return lookupValue;
     } else {
-        const newInstance = new modelConfiguration[typeDefinition.type]();
-        modelConfiguration[typeDefinition.type].values.push(newInstance);
-        newInstance.effects = [...modelConfiguration[typeDefinition.type].prototype.effects];
+        const newInstance = new modelConfiguration[instanceType]();
+        modelConfiguration[instanceType].values.push(newInstance);
+        newInstance.effects = [...modelConfiguration[instanceType].prototype.effects];
         return newInstance;
     }
 }
